b97092ab35a9240977c4701e60048bf02339d625
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-08 13:56:22 +0700

    Todo List Project_10. Todo List Component

--- 5. Todo List Project/todo-list-project/src/App.js
+++ 5. Todo List Project/todo-list-project/src/App.js
@@ -3,7 +3,6 @@ import uuid from 'react-uuid';
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
-// showing vs-code github setup
 class App extends Component {
   state = {
     items: [],
--- 5. Todo List Project/todo-list-project/src/components/TodoInput.js
+++ 5. Todo List Project/todo-list-project/src/components/TodoInput.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./todoinput.css";
+import '../components/todoinput.css';
 
 export default class TodoInput extends Component {
   render() {
@@ -10,7 +10,7 @@ export default class TodoInput extends Component {
           <div className="input-group">
             <div className="input-group-prepend">
               <div className="input-group-text bg-primary text-white">
-                {/* <i className="fas fa-book" /> */}
+               
               </div>
             </div>
             <input
