d55e8e54b28622cac239e97e52c87f62ee5e0407
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-08 11:56:23 +0700

    React Basics Tutorial Continued_5. Alternative State Syntax

--- 2. React Basics Tutorial Continued/my-app/src/Booklist.js
+++ 2. React Basics Tutorial Continued/my-app/src/Booklist.js
@@ -1,26 +1,17 @@
 import React, { Component } from "react";
 import Book from "./Book";
+import booksData from "./bookData";
 export default class Booklist extends Component {
-  state = {
-    books: [
-      { id: 1, book: "book number one", author: "john doe" },
-      {
-        id: 2,
-        book: "book number two",
-        author: "bobby doe"
-      }
-    ]
-  };
-  //this.setState({})
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: booksData
+    };
+  }
+  // state = {
+
+  // }
   render() {
-    // const books = this.state.books.map(item => item.book);
-    // console.log(books);
-
-    //filter
-    //forEach
-    //map
-    //reduce
-
     return (
       <section>
         <h3>This is our Booklist</h3>
---
+++ 2. React Basics Tutorial Continued/my-app/src/bookData.js
@@ -0,0 +1,25 @@
+const books = [
+  { id: 1, book: "book number one", author: "john doe" },
+  {
+    id: 2,
+    book: "book number two",
+    author: "bobby doe"
+  },
+  {
+    id: 3,
+    book: "book number two",
+    author: "bobby doe"
+  },
+  {
+    id: 4,
+    book: "book number two",
+    author: "bobby doe"
+  },
+  {
+    id: 5,
+    book: "book number two",
+    author: "bobby doe"
+  }
+];
+
+export default books;
