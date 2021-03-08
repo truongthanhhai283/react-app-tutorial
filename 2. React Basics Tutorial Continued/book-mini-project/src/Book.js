8dfe90f78f4d849983d792a86123cf10ba8baad4
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-08 12:01:08 +0700

    React Basics Tutorial Continued_7. Events

--- 2. React Basics Tutorial Continued/book-mini-project/src/Book.js
+++ 2. React Basics Tutorial Continued/book-mini-project/src/Book.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
 
 export default class Book extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1
+    };
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  // handleClick() {
+  //   console.log("you clicked me");
+  //   console.log(this.state.count);
+  // }
+  handleClick = () => {
+    console.log("you clicked me");
+    console.log(this.state.count);
+  };
+
   render() {
     // console.log(this.props);
+
     const { img, title, author } = this.props.info;
     return (
       <article className="book">
@@ -10,6 +28,9 @@ export default class Book extends Component {
         <div>
           <h4>Title : {title}</h4>
           <h6>Author : {author}</h6>
+          <button type="button" onClick={this.handleClick}>
+            add count
+          </button>
         </div>
       </article>
     );
