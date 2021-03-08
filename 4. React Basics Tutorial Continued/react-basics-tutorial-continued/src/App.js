2b753e67a8494a9ec369f25ae9782dca51d592df
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-08 12:44:09 +0700

    React Basics Tutorial Continued_3. PropTypes  Object Shape

--- 4. React Basics Tutorial Continued/react-basics-tutorial-continued/src/App.js
+++ 4. React Basics Tutorial Continued/react-basics-tutorial-continued/src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
 import PropTypes from "prop-types";
+// object with particular shape
 
-const Person = ({ img, name, age, info }) => {
+const Person = ({ person: { img, name, age, info } }) => {
+  // const { img, name, age, info} = props.person
+  // console.log(info);
   return (
     <article>
       <img src={img} alt="person" />
       <h4>name : {name}</h4>
       <h4>age : {age}</h4>
-      <h4>info : {info}</h4>
+      <h4>info : {info || "default info about the person"}</h4>
     </article>
   );
 };
 Person.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired
+  person: PropTypes.shape({
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    info: PropTypes.string.isRequired
+  })
 };
 Person.defaultProps = {
-  img: "https://randomuser.me/api/portraits/thumb/men/45.jpg",
-  name: "james doe",
-  age: 34,
-  info: "default info about person"
+  person: {
+    info: "default info about the person"
+  }
 };
 class PersonList extends Component {
   state = {
@@ -34,8 +39,8 @@ class PersonList extends Component {
       },
       {
         id: 2,
-        // img: "https://randomuser.me/api/portraits/thumb/men/34.jpg",
-        // name: "bob",
+        img: "https://randomuser.me/api/portraits/thumb/men/34.jpg",
+        name: "bob",
         age: 27
       },
       {
@@ -51,13 +56,7 @@ class PersonList extends Component {
     return (
       <section>
         {this.state.people.map(person => (
-          <Person
-            key={person.id}
-            img={person.img}
-            name={person.name}
-            age={person.age}
-            info={person.info}
-          />
+          <Person key={person.id} person={person} />
         ))}
       </section>
     );
