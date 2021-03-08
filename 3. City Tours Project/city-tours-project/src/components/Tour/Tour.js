be5955fcbdf6f9a1f22332e0f5322a69a35864c0
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-08 12:28:21 +0700

    City Tours Project_8. TourList Component - 1

--- 3. City Tours Project/city-tours-project/src/components/Tour/Tour.js
+++ 3. City Tours Project/city-tours-project/src/components/Tour/Tour.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
-
+import "./tour.scss";
 export default class Tour extends Component {
   render() {
-    return <h1>Hello from tour</h1>;
+    const { id, city, img, name, info } = this.props.tour;
+    const { removeTour } = this.props;
+
+    return (
+      <article className="tour">
+        <div className="img-container">
+          <img src={img} alt="image of the tour" />
+          <span className="close-btn">
+            <i className="fas fa-window-close" />
+          </span>
+        </div>
+        <div className="tour-info">
+          <h3>{city}</h3>
+          <h4>{name}</h4>
+          <h5>
+            info{" "}
+            <span>
+              <i className="fas fa-caret-square-down" />
+            </span>
+          </h5>
+          <p>{info}</p>
+        </div>
+      </article>
+    );
   }
 }
--- 3. City Tours Project/city-tours-project/src/components/TourList/index.js
+++ 3. City Tours Project/city-tours-project/src/components/TourList/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import Tour from "../Tour";
+import "./tourlist.scss";
+import { tourData } from "../../tourData";
+
 export default class TourList extends Component {
+  state = {
+    tours: tourData
+  };
+  removeTour = id => {
+    console.log(id);
+  };
   render() {
+    // console.log(this.state.tours);
+    const { tours } = this.state;
+
     return (
-      <div>
-        Hello From Tourslist
-        <Tour />
-      </div>
+      <section className="tourlist">
+        {tours.map(tour => (
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+        ))}
+      </section>
     );
   }
 }
