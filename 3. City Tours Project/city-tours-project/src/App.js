4b2700c0c39d6a83b11296a8b18fff561c63ce66
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-08 12:26:58 +0700

    City Tours Project_5. Navbar Component Logic + City Tours Project_6. Navbar Component SASS

--- 3. City Tours Project/city-tours-project/src/App.js
+++ 3. City Tours Project/city-tours-project/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import "./App.scss";
-
+import Navbar from "./components/Navbar/Navbar";
 class App extends Component {
   render() {
-    return <h1>our current app</h1>;
+    return (
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
+    );
   }
 }
 
