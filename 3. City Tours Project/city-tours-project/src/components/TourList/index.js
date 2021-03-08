f91edbc8432c98cdc3db41ef49f9619369137e94
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-08 12:31:01 +0700

    City Tours Project_12. Tour Component Delete

--- 3. City Tours Project/city-tours-project/src/components/TourList/index.js
+++ 3. City Tours Project/city-tours-project/src/components/TourList/index.js
@@ -8,7 +8,11 @@ export default class TourList extends Component {
     tours: tourData
   };
   removeTour = id => {
-    console.log(id);
+    const { tours } = this.state;
+    const sortedTours = tours.filter(tour => tour.id !== id);
+    this.setState({
+      tours: sortedTours
+    });
   };
   render() {
     // console.log(this.state.tours);
