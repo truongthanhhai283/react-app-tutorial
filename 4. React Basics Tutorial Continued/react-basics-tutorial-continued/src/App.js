cbf3977da43e49e61ae32f0e6eca5e59de08ea69
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-08 12:47:08 +0700

    React Basics Tutorial Continued_4. Controlled Inputs and Form Submission

--- 4. React Basics Tutorial Continued/react-basics-tutorial-continued/src/App.js
+++ 4. React Basics Tutorial Continued/react-basics-tutorial-continued/src/App.js
@@ -1,63 +1,74 @@
 import React, { Component } from "react";
 import "./App.css";
-import PropTypes from "prop-types";
-// object with particular shape
+// controlled uncontrolled inputs
+// JS
+// const input = document.getElementById('myText');
+// const inputValue = input.value;
+// React
+// value, onChange
 
-const Person = ({ person: { img, name, age, info } }) => {
-  // const { img, name, age, info} = props.person
-  // console.log(info);
-  return (
-    <article>
-      <img src={img} alt="person" />
-      <h4>name : {name}</h4>
-      <h4>age : {age}</h4>
-      <h4>info : {info || "default info about the person"}</h4>
-    </article>
-  );
-};
-Person.propTypes = {
-  person: PropTypes.shape({
-    img: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    age: PropTypes.number.isRequired,
-    info: PropTypes.string.isRequired
-  })
-};
-Person.defaultProps = {
-  person: {
-    info: "default info about the person"
-  }
-};
-class PersonList extends Component {
+class Form extends Component {
   state = {
-    people: [
-      {
-        id: 1,
-        img: "https://randomuser.me/api/portraits/thumb/men/65.jpg",
-        name: "john",
-        age: 24
-      },
-      {
-        id: 2,
-        img: "https://randomuser.me/api/portraits/thumb/men/34.jpg",
-        name: "bob",
-        age: 27
-      },
-      {
-        id: 3,
-        img: "https://randomuser.me/api/portraits/thumb/men/87.jpg",
-        name: "peter",
-        age: 27,
-        info: "some info about peter"
-      }
-    ]
+    firstName: "",
+    lastName: "",
+    people: []
   };
+  handleChange = event => {
+    // console.log(event.target);
+    // console.log(event.target.name);
+    // console.log(event.target.value);
+
+    // if (event.target.name === "firstName") {
+    //   const textValue = event.target.value;
+
+    //   this.setState({
+    //     firstName: textValue
+    //   });
+    // }
+
+    this.setState({
+      [event.target.name]: [event.target.value]
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const firstName = this.state.firstName;
+    const lastName = this.state.lastName;
+    if (firstName.length > 0 && lastName.length > 0) {
+      const person = ` ${firstName} ${lastName} `;
+      this.setState({
+        people: [...this.state.people, person],
+        firstName: "",
+        lastName: ""
+      });
+    }
+  };
+
   render() {
     return (
       <section>
-        {this.state.people.map(person => (
-          <Person key={person.id} person={person} />
-        ))}
+        <article>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleChange}
+            />
+            <input
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleChange}
+            />
+            <button type="submit">submit</button>
+          </form>
+        </article>
+        <article>
+          <h1>people</h1>
+          <div>{this.state.people}</div>
+        </article>
       </section>
     );
   }
@@ -65,7 +76,7 @@ class PersonList extends Component {
 
 class App extends Component {
   render() {
-    return <PersonList />;
+    return <Form />;
   }
 }
 
