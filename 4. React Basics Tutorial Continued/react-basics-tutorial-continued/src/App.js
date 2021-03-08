5102cc73b6b5843875d5d352bbc0d1d094f77a98
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-08 12:54:17 +0700

    React Basics Tutorial Continued_6. Uncontrolled Inputs with ref

--- 4. React Basics Tutorial Continued/react-basics-tutorial-continued/src/App.js
+++ 4. React Basics Tutorial Continued/react-basics-tutorial-continued/src/App.js
@@ -3,68 +3,27 @@ import "./App.css";
 //uncontrolled inputs
 
 class Form extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    people: []
-  };
-  handleChange = event => {
-    // console.log(event.target);
-    // console.log(event.target.name);
-    // console.log(event.target.value);
-
-    // if (event.target.name === "firstName") {
-    //   const textValue = event.target.value;
-
-    //   this.setState({
-    //     firstName: textValue
-    //   });
-    // }
-    // const value = event.target.value.toUpperCase();
-    const value = event.target.value;
-    this.setState({
-      [event.target.name]: value
-    });
-  };
-
   handleSubmit = e => {
     e.preventDefault();
-    const firstName = this.state.firstName;
-    const lastName = this.state.lastName;
-    if (firstName.length > 0 && lastName.length > 0) {
-      const person = ` ${firstName} ${lastName} `;
-      this.setState({
-        people: [...this.state.people, person],
-        firstName: "",
-        lastName: ""
-      });
-    }
-  };
+    const name = this.refs.myName;
+    const nameValue = name.value;
 
+    const email = this.email.value;
+
+    const text = this.refs.myText;
+    const textValue = text.textContent;
+    text.style.color = "red";
+    console.log(nameValue, email, textValue);
+  };
   render() {
     return (
       <section>
-        <article>
-          <form onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              name="firstName"
-              value={this.state.firstName}
-              onChange={this.handleChange}
-            />
-            <input
-              type="text"
-              name="lastName"
-              value={this.state.lastName}
-              onChange={this.handleChange}
-            />
-            <button type="submit">submit</button>
-          </form>
-        </article>
-        <article>
-          <h1>people</h1>
-          <div>{this.state.people}</div>
-        </article>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" ref="myName" />
+          <input type="email" ref={orange => (this.email = orange)} />
+          <button type="submit">submit</button>
+        </form>
+        <p ref="myText">hello world</p>
       </section>
     );
   }
