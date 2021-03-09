6775bd1c1c7de55a0360c5e31fef27bdf7091745
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-09 08:30:52 +0700

    Tech Store_7. Folder and Page Structure

--- 7. Tech Store/react-app-tech-store/src/App.js
+++ 7. Tech Store/react-app-tech-store/src/App.js
@@ -1,32 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import styled from "styled-components";
 
-// import {BsFillChatSquareDotsFill} from 'react-icons/fa';
-import { BsFillChatSquareDotsFill } from "react-icons/bs";
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Products from "./pages/ProductsPage";
+import Contact from "./pages/ContactPage";
+import SingleProduct from "./pages/SingleProductPage";
+import Default from "./pages/Default.js";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <h1>
-          hello from tech store
-          <BsFillChatSquareDotsFill />
-        </h1>
-        <Button large>Hello World</Button><br></br>
-        <Button>Hello World</Button>
-      </div>
-    );
+    return <h1>hello from tech store</h1>;
   }
 }
-
-const color = "#999";
-const Button = styled.button`
-  color: ${color};
-  background-color: black;
-  /* font-size:2em; */
-  font-size: ${(props) => (props.large ? "3rem" : "1rem")};
-`;
 
 export default App;
