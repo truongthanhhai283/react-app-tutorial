95d91df684cb717aceab44e847f06014583117d3
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-09 09:08:48 +0700

    Tech Store_34. SetSingle Product Method

--- 7. Tech Store/react-app-tech-store/src/context/context.js
+++ 7. Tech Store/react-app-tech-store/src/context/context.js
@@ -20,7 +20,7 @@ class ProductProvider extends Component {
     filteredProducts: [],
     featuredProducts: [],
     singleProduct: {},
-    loading: false
+    loading: true
   };
   componentDidMount() {
     //from contentful items
@@ -65,7 +65,9 @@ class ProductProvider extends Component {
   };
   // get product from local storage
   getStorageProduct = () => {
-    return {};
+    return localStorage.getItem("singleProduct")
+      ? JSON.parse(localStorage.getItem("singleProduct"))
+      : {};
   };
   // get totals
   getTotals = () => {
@@ -130,7 +132,12 @@ class ProductProvider extends Component {
   };
   // set single product
   setSingleProduct = id => {
-    console.log(`set single product ${id}`);
+    let product = this.state.storeProducts.find(item => item.id === id);
+    localStorage.setItem("singleProduct", JSON.stringify(product));
+    this.setState({
+      singleProduct: { ...product },
+      loading: false
+    });
   };
 
   // handle sidebar
