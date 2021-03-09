9e0a35098d9833850f76a18aeb68ff104d893607
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-09 09:14:37 +0700

    Tech Store_39. CartPage Setup

--- 7. Tech Store/react-app-tech-store/src/components/CartPage/Cart.js
+++ 7. Tech Store/react-app-tech-store/src/components/CartPage/Cart.js
@@ -5,17 +5,10 @@ import CartList from "./CartList";
 import CartTotals from "./CartTotals";
 export default function Cart() {
   return (
-    <section className="py-5">
-      {/* title */}
-      <div className="container">
-        <Title title="your cart items" center />
-      </div>
-      {/* cart columns */}
+    <div>
       <CartColumns />
-      {/* cart list */}
       <CartList />
-      {/* cart totals */}
       <CartTotals />
-    </section>
+    </div>
   );
 }
