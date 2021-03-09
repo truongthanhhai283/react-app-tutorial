d915f29bbbdeed20b7674513b427b25e21b8cbee
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-09 08:36:22 +0700

    Tech Store_8. React Router Setup

--- 7. Tech Store/react-app-tech-store/src/App.js
+++ 7. Tech Store/react-app-tech-store/src/App.js
@@ -7,11 +7,27 @@ import About from "./pages/AboutPage";
 import Products from "./pages/ProductsPage";
 import Contact from "./pages/ContactPage";
 import SingleProduct from "./pages/SingleProductPage";
+import Cart from "./pages/CartPage.js";
 import Default from "./pages/Default.js";
+
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
-    return <h1>hello from tech store</h1>;
+    return (
+      <>
+        {/* navbar, sidebar, cart,footer*/}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/products/:id" component={SingleProduct} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </>
+    );
   }
 }
 
