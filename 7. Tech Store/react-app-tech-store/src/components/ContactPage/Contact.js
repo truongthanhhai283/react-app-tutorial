d3c0ba139b86a47cb49278aa3ab0e1dd654d0089
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-09 08:50:05 +0700

    Tech Store_21. Formspree

--- 7. Tech Store/react-app-tech-store/src/components/ContactPage/Contact.js
+++ 7. Tech Store/react-app-tech-store/src/components/ContactPage/Contact.js
@@ -6,7 +6,11 @@ export default function Contact() {
       <div className="row">
         <div className="col-10 mx-auto col-md-6 my-3">
           <Title title="contact us" />
-          <form className="mt-5">
+          <form
+            className="mt-5"
+            action="https://formspree.io/learncodetutorial@gmail.com"
+            method="POST"
+          >
             {/* first */}
             <div className="form-group">
               <input
@@ -41,6 +45,14 @@ export default function Contact() {
                 className="form-control"
                 rows="10"
                 placeholder="hello there buddy"
+              />
+            </div>
+            {/* submit */}
+            <div className="form-group mt-3">
+              <input
+                type="submit"
+                value="Send"
+                className="form-control bg-primary text-white"
               />
             </div>
           </form>
